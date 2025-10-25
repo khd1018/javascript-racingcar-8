9bc0e74d16f1f6ccdd398241343584b2aba37aba
hyeongdong <khd941018@gmail.com>
2025-10-26 02:08:28 +0900

    refactor(utils): 자동차 이름 검증 기능 NameValidator로 분리
    기존 InputValidator에는 입력값 형식을 검증하는 기능들과 자동차 이름
    규칙에 대한 검증 기능이 혼재되어 있었고, 이로 인해 해당 클래스가 복잡했기 때문에
    책임 분리와 가독성을 위해 자동차 이름 검증 기능들을 NameValidator로 분리

--- src/utils/InputValidator.js
+++ src/utils/InputValidator.js
@@ -18,26 +18,6 @@ class InputValidator {
       throw new Error("[ERROR] 이름을 쉼표(,)로 구분해서 입력해주세요. ");
     }
   }
-
-  static checkDuplication(names) {
-    const nameSet = new Set(names);
-
-    if (nameSet.size() !== names.length) {
-      throw new Error("[ERROR] 서로 다른 이름을 입력해주세요. ");
-    }
-  }
-
-  static checkMinimum(names) {
-    if (names.length < 2) {
-      throw new Error("[ERROR] 이름을 2개 이상 입력해주세요. ");
-    }
-  }
-
-  static checkMaximum(names) {
-    if (names.length > 100) {
-      throw new Error("[ERROR] 이름을 2개 이상 입력해주세요. ");
-    }
-  }
 }
 
 export default InputValidator;
---
+++ src/utils/NameValidator.js
@@ -0,0 +1,23 @@
+class NameValidator {
+  static checkDuplication(names) {
+    const nameSet = new Set(names);
+
+    if (nameSet.size() !== names.length) {
+      throw new Error("[ERROR] 서로 다른 이름을 입력해주세요. ");
+    }
+  }
+
+  static checkMinimum(names) {
+    if (names.length < 2) {
+      throw new Error("[ERROR] 이름을 2개 이상 입력해주세요. ");
+    }
+  }
+
+  static checkMaximum(names) {
+    if (names.length > 100) {
+      throw new Error("[ERROR] 이름을 2개 이상 입력해주세요. ");
+    }
+  }
+}
+
+export default NameValidator;
