c701e89a98e2b9747592cd8a04847048811d5947
hyeongdong <khd941018@gmail.com>
2025-10-26 01:49:19 +0900

    feat(InputValidator): 이름이 2개이상 입력되었는지 확인하는 기능 구현

--- src/utils/InputValidator.js
+++ src/utils/InputValidator.js
@@ -19,7 +19,13 @@ class InputValidator {
     }
   }
 
-  check;
+  checkMinimum(names) {
+    if (names.length < 2) {
+      throw new Error("[ERROR] 이름을 2개 이상 입력해주세요. ");
+    }
+  }
+
+  checkMaximum(name) {}
 }
 
 export default InputValidator;
