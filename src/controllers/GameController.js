09bc19d0316a8ddb83240c89c270e3d91b7d0022
hyeongdong <khd941018@gmail.com>
2025-10-27 01:10:53 +0900

    feat(Controller): 게임 실행에 필요한 로직 구현

---
+++ src/controllers/GameController.js
@@ -0,0 +1,50 @@
+import InputValidator from "../utils/InputValidator";
+import NameValidator from "../utils/NameValidator";
+import RacingGame from "../models/RacingGame";
+import CarFactory from "../factories/CarFactory";
+
+class GameController {
+  #racingGame;
+
+  initialize(nameInput, finalRoundInput) {
+    this.#validateInputs(nameInput, finalRoundInput);
+    const cars = CarFactory.createCars(this.#parseNames(nameInput));
+    this.#racingGame = new RacingGame(Number(finalRoundInput), cars);
+  }
+
+  runGame() {
+    this.#racingGame.run();
+  }
+
+  getGameResult() {
+    return this.#racingGame.getCarStates();
+  }
+
+  getGameWinners() {
+    const farthestCar = this.#racingGame.findFarthestCar();
+    return this.#racingGame.getWinners(farthestCar);
+  }
+
+  #parseNames(nameInput) {
+    const names = nameInput.split(",");
+    this.#validateNames(names);
+    return names;
+  }
+
+  #validateInputs(nameInput, finalRoundInput) {
+    InputValidator.checkEmpty(nameInput);
+    InputValidator.checkDelimiter(nameInput);
+
+    InputValidator.checkEmpty(finalRoundInput);
+    InputValidator.checkNumber(finalRoundInput);
+  }
+
+  #validateNames(names) {
+    NameValidator.checkDuplication(names);
+    NameValidator.checkMinimum(names);
+    NameValidator.checkMaximum(names);
+    NameValidator.checkEmptyString(names);
+  }
+}
+
+export default GameController;
