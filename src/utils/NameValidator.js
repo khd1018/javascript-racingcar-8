3c492a6587dea7d0a5c697b9e1935e9c9ad360b9
hyeongdong <khd941018@gmail.com>
2025-10-26 18:28:51 +0900

    chore(NameValidator) : 오타 수정

--- src/utils/NameValidator.js
+++ src/utils/NameValidator.js
@@ -2,7 +2,7 @@ class NameValidator {
   static checkDuplication(names) {
     const nameSet = new Set(names);
 
-    if (nameSet.size() !== names.length) {
+    if (nameSet.size !== names.length) {
       throw new Error("[ERROR] 서로 다른 이름을 입력해주세요. ");
     }
   }
@@ -15,7 +15,7 @@ class NameValidator {
 
   static checkMaximum(names) {
     if (names.length > 100) {
-      throw new Error("[ERROR] 이름을 2개 이상 입력해주세요. ");
+      throw new Error("[ERROR] 이름을 100개 이하로 입력해주세요. ");
     }
   }
 }
