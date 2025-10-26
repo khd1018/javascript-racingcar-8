5bbcc205d5c0b5f712d4301f2820782db872d372
hyeongdong <khd941018@gmail.com>
2025-10-26 18:29:29 +0900

    test(NameValidator): NameValidator 테스트 코드 구현

---
+++ __tests__/NameValidatorTest.js
@@ -0,0 +1,21 @@
+import NameValidator from "../src/utils/NameValidator";
+
+describe("NameValidator 테스트", () => {
+  test("중복 이름이 있으면 Error를 발생시킬 수 있다. ", () => {
+    const names = ["messi", "ronaldo", "messi"];
+
+    expect(() => NameValidator.checkDuplication(names)).toThrow("[ERROR]");
+  });
+
+  test("이름이 하나이면 Error를 발생시킬 수 있다.", () => {
+    const names = ["messi"];
+
+    expect(() => NameValidator.checkMinimum(names)).toThrow("[ERROR]");
+  });
+
+  test("이름이 5개 이상이면 Error를 발생시킬 수 있다.", () => {
+    const names = new Array(101).fill("apple");
+
+    expect(() => NameValidator.checkMaximum(names)).toThrow("[ERROR]");
+  });
+});
