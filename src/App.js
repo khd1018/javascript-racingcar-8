4522d2d76b0e0f84147394c2663f3ac3f37a9968
hyeongdong <khd941018@gmail.com>
2025-10-27 01:39:36 +0900

    feat(App): 게임 입출력과 실행 코드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import GameController from "./controllers/GameController";
+import InputView from "./views/InputView";
+import OutputView from "./views/OutputView";
+
 class App {
-  async run() {}
+  async run() {
+    const nameInput = await InputView.askInput();
+    const finalRoundInput = await InputView.askInput();
+    const gameController = new GameController();
+
+    gameController.initialize(nameInput, finalRoundInput);
+    gameController.runGame();
+    OutputView.printResult(gameController.getGameResult());
+    OutputView.printWinners(gameController.getGameWinners());
+  }
 }
 
 export default App;
