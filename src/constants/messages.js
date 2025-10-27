2a0f1c93c699a20bb18a8353abcd1445a148d353
hyeongdong <khd941018@gmail.com>
2025-10-27 21:18:23 +0900

    refactor(messages): 하드코딩된 값들을 상수로 분리

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,3 +1,5 @@
+import { GAME_CONFIG } from "./gameConfig.js";
+
 const ERROR_PREFIX = "[ERROR]";
 
 const MESSAGE = {
@@ -12,11 +14,11 @@ const ERROR_MESSAGE = {
   NOT_A_NUMBER: `${ERROR_PREFIX} 숫자를 입력해주세요.`,
   DUPLICATE_NAME: `${ERROR_PREFIX} 서로 다른 이름을 입력해주세요. `,
   EMPTY_NAME: `${ERROR_PREFIX}이름을 입력해주세요. `,
-  TOO_FEW_NAMES: `${ERROR_PREFIX} 이름을 2개 이상 입력해주세요. `,
-  TOO_MANY_NAMES: `${ERROR_PREFIX} 이름을 100개 이하로 입력해주세요. `,
-  INVALID_DELIMITER: `${ERROR_PREFIX} 이름을 쉼표(,)로 구분해서 입력해주세요. `,
-  INVALID_NAME_LENGTH: `${ERROR_PREFIX} 자동차 이름은 5자 이하만 가능합니다.`,
-  INVALID_FINAL_ROUND: `${ERROR_PREFIX} 시도할 횟수는 1이상 100이하 숫자만 입력해주세요.`,
+  TOO_FEW_NAMES: `${ERROR_PREFIX} 이름을 ${GAME_CONFIG.MIN_NAME_COUNT}개 이상 입력해주세요. `,
+  TOO_MANY_NAMES: `${ERROR_PREFIX} 이름을 ${GAME_CONFIG.MAX_NAME_COUNT}개 이하로 입력해주세요. `,
+  INVALID_DELIMITER: `${ERROR_PREFIX} 이름을 쉼표(${GAME_CONFIG.NAME_DELIMITER})로 구분해서 입력해주세요. `,
+  INVALID_NAME_LENGTH: `${ERROR_PREFIX} 자동차 이름은 ${GAME_CONFIG.MAX_NAME_LENGTH}자 이하만 가능합니다.`,
+  INVALID_FINAL_ROUND: `${ERROR_PREFIX} 시도할 횟수는 ${GAME_CONFIG.MIN_ROUND}이상 ${GAME_CONFIG.MAX_ROUND}이하 숫자만 입력해주세요.`,
 };
 
 export { MESSAGE, ERROR_MESSAGE };
